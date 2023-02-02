bcd988e943d9df948095a004eb3ddc6a2a1855f4
Abiel Isak <abizmo99@gmail.com>
2023-02-02 18:09:06 +0100

    Borja & Abiel

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose');
-
+mongoose.set('strictQuery', true)
 // Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data.json");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
+const MONGODB_URI = 'mongodb://127.0.0.1/recipe-app';
 
 // Connection to the database "recipe-app"
 mongoose
@@ -17,7 +17,29 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    Recipe.create(data)
+    .then(recipeDetails =>{
+      for (let i = 0; i < recipeDetails.length; i++) {
+
+        console.log("recipeDetails....",recipeDetails[i].title);
+      }
+    })
+    
+    // Recipe.insertMany(data)
+    // .then(recipeArrDB =>{
+
+    //   console.log("recipeArr....",recipeArrDB.title);
+    // //   for (let i = 0; i < recipeArr.length; i++) {
+
+    // //   }
+    // // })
+
+    Recipe.findByIdAndUpdate("63dbe8a3de320166f357d6c4",{duration:100}, {new:true})
+    .then(uptadedDuration =>{
+      console.log("Succefully modified....",uptadedDuration);
+    })
   })
+
   .catch(error => {
     console.error('Error connecting to the database', error);
-  });
+  })
